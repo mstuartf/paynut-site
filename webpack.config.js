e0ac59621fa0f975e473b229a9ca759d03b65842
mikef <mstuartflood@gmail.com>
2019-12-29 18:36:36 +0000

    second page added

--- webpack.config.js
+++ webpack.config.js
@@ -5,48 +5,73 @@ const CustomExtractor = require('./purgecss.extractor');
 
 module.exports = {
 
-  mode: process.env.NODE_ENV,
+    mode: process.env.NODE_ENV,
 
-  entry: ['./src/styles.css', './src/main.ts'],
+    entry: {
+        main: "./src/main.ts",
+        styles: "./src/styles.css",
+        register: "./src/register.ts"
+    },
 
-  module: {
+    output: {
+        filename: "./[name].bundle.js"
+    },
 
-    rules: [
+    module: {
 
-      {
-          test: /\.ts$/,
-          loader: 'ts-loader',
-          exclude: /node_modules/,
-      },
+        rules: [
 
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [ 'css-loader', 'postcss-loader'],
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'postcss-loader'],
+                }),
+            },
+
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {name: '[name].[ext]?[hash]'}
+            }
+
+        ]
+
+    },
+
+    plugins: [
+
+        // don't inline styles in the JS bundle but make a separate CSS bundle
+        new ExtractTextPlugin('styles.css'),
+
+        // build a file for the register page importing styles and the register bundle
+        new HtmlWebpackPlugin({
+            hash: true,
+            template: './src/index.html',
+            chunks: ['main', 'styles'],
+            filename: './index.html'
         }),
-      },
 
-      {
-          test: /\.(png|jpg|gif|svg)$/,
-          loader: 'file-loader',
-          options: { name: '[name].[ext]?[hash]' }
-      }
+        // build a file for the register page importing styles and the register bundle
+        new HtmlWebpackPlugin({
+            hash: true,
+            template: './src/register.html',
+            chunks: ['register', 'styles'],
+            filename: './register.html'
+        }),
+
+        // use Purge CSS to filter out unused styles to reduce the CSS bundle size
+        new PurgecssPlugin({
+            paths: ['src/index.html', 'src/register.html'],
+            extractors: [{extractor: CustomExtractor, extensions: ['css', 'html']}]
+        })
 
     ]
-    
-  },
 
-  plugins: [
-
-    // don't inline styles in the JS bundle but make a separate CSS bundle
-    new ExtractTextPlugin('styles.css'),
-
-    // build an index.html file in the dist folder
-    new HtmlWebpackPlugin({ template: 'src/index.html', favicon:  'src/favicon.ico' }),
-
-    // use Purge CSS to filter out unused styles to reduce the CSS bundle size
-    new PurgecssPlugin({paths: ['src/index.html'], extractors: [{extractor: CustomExtractor, extensions: ['css', 'html']}]})
-
-  ]
-}
+};
