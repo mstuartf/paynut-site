15cabc1172f36608a0681557343fae9bcdc57767
mikef <mstuartflood@gmail.com>
2019-12-29 17:06:03 +0000

    screenshots

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 // import all images here
 import './assets/logo.png';
+import './assets/frame.png';
+import './assets/iphone_detail.png';
+import './assets/ipad_list.png';
 
 
 function placeholderFunction(arg: string) {
